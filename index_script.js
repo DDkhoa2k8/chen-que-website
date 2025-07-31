4d0da768211435b7057244fb3f5363da780153e2
DDKhoa2k8 <ddkmgb999@gmail.com>
2025-07-31 16:58:19 +0700

    update: fix req spam

--- index_script.js
+++ index_script.js
@@ -1,12 +1,25 @@
 //Ruou
-const frameCount = 17;
+const frameCount = 16;
 const endScroll = 500;
 const ruouImg = document.getElementById("ruou");
+const ruouImgCon = document.getElementById("ruou-img-con");
+const ruouImgList = [];
 
-window.onload = function () {
+async function preLoadRuou() {
+    for (let i = 1; i <= frameCount;i++) {
+        const img = new Image();
+        img.src = `video/ruou${i}.png`;
+        ruouImgList.push(img);
+    }
+}
+
+window.onload = async function () {
+    await preLoadRuou();
+
     window.addEventListener('scroll', e => { 
         if (window.scrollY < endScroll) {
-            ruouImg.src = "video/ruou" + Math.floor((1 - window.scrollY / endScroll) * 16) + ".png";
+            ruouImgCon.innerHTML = "";
+            ruouImgCon.appendChild(ruouImgList[Math.floor((1 - window.scrollY / endScroll) * (frameCount - 1))]);
         }
     });
 };
